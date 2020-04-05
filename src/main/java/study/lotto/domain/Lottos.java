c433a616a60e54b658fd64f7b9c6c43d1df1fb5f
luvram <sahul0804@gmail.com>
2020-04-05 11:20:35 +0900

    refactor: 자료형 형태의 변수명 변경

--- src/main/java/study/lotto/domain/Lottos.java
+++ src/main/java/study/lotto/domain/Lottos.java
@@ -5,42 +5,42 @@ import java.util.function.Function;
 import java.util.stream.Collectors;
 
 public class Lottos implements Iterable<Lotto> {
-    private List<Lotto> list;
+    private List<Lotto> lottos;
 
     public Lottos() {
-        this.list = new ArrayList<>();
+        this.lottos = new ArrayList<>();
     }
 
-    public Lottos(List<Lotto> list) {
-        this.list = new ArrayList<>(list);
+    public Lottos(List<Lotto> lottos) {
+        this.lottos = new ArrayList<>(lottos);
     }
 
-    public Lottos(Lottos list) {
-        this(list.list);
+    public Lottos(Lottos lottos) {
+        this(lottos.lottos);
     }
 
     public int size() {
-        return list.size();
+        return lottos.size();
     }
 
     protected void addAll(List<Lotto> lottos) {
-        this.list.addAll(lottos);
+        this.lottos.addAll(lottos);
     }
 
     public Lotto get(int index) {
-        return list.get(index);
+        return lottos.get(index);
     }
 
     public WinningLottos ranks(LottoWinningNumber lottoWinningNumber) {
         Function<Lotto, LottoRank> key =
                 lotto -> lotto.rank(lottoWinningNumber);
         Map<LottoRank, List<Lotto>> lottoRanks =
-                list.stream().collect(Collectors.groupingBy(key));
+                lottos.stream().collect(Collectors.groupingBy(key));
 
         return new WinningLottos(lottoRanks);
     }
 
     @Override public Iterator<Lotto> iterator() {
-        return list.iterator();
+        return lottos.iterator();
     }
 }
